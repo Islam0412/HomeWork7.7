9f47c07e60a84fa8049a785265230f7bf6a03c9f
Islam0412 <islamlivaza2@gmail.com>
2022-08-21 19:13:07 +0600

    ltyko;

--- src/Hero.java
+++ src/Hero.java
@@ -4,5 +4,29 @@ public abstract class Hero implements HavingSuperAbility {
     public String supper;
 
 
+    public int getHealt() {
+        return healt;
+    }
+
+    public void setHealt(int healt) {
+        this.healt = healt;
+    }
+
+    public String getSupper() {
+        return supper;
+    }
+
+    public void setSupper(String supper) {
+        this.supper = supper;
+    }
+
+    public int getDamag() {
+        return damag;
+    }
+
+    public void setDamag(int damag) {
+        this.damag = damag;
+    }
 }
+
 
--- src/Medic.java
+++ src/Medic.java
@@ -10,5 +10,5 @@ public class Medic extends Hero{
     }
    public int HealPoints;
     double increaseExperience;
-    
+
 }
